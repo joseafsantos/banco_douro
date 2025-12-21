5276fe942c0e636e66b12402eeafa6eb5d17d05d
Jose A F Santos <toni.farias@outlook.com>
2025-12-21 11:36:43 -0300

    lista de contas implementada e configurada

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:banco_douro/models/account.dart';
+import 'package:banco_douro/services/account_service.dart';
 import 'package:banco_douro/ui/styles/app_colors.dart';
+import 'package:banco_douro/ui/widgets/account_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -18,6 +21,36 @@ class HomeScreen extends StatelessWidget {
             icon: Icon(Icons.logout),
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+          future: AccountService().getAll(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return Center(child: CircularProgressIndicator());
+              case ConnectionState.waiting:
+                return Center(child: CircularProgressIndicator());
+              case ConnectionState.active:
+                return Center(child: CircularProgressIndicator());
+              case ConnectionState.done:
+                {
+                  if (snapshot.data == null || snapshot.data!.isEmpty) {
+                    return Center(child: Text("Nenhuma conta recebida."));
+                  } else {
+                    List<Account> listAccounts = snapshot.data!;
+                    return ListView.builder(
+                      itemCount: listAccounts.length,
+                      itemBuilder: (context, index) {
+                        return AccountWidget(account: listAccounts[index]);
+                      },
+                    );
+                  }
+                }
+            }
+          },
+        ),
       ),
     );
   }
---
+++ lib/ui/widgets/account_widget.dart
@@ -0,0 +1,41 @@
+import 'package:banco_douro/ui/styles/app_colors.dart';
+
+import '../../models/account.dart';
+import 'package:flutter/material.dart';
+
+class AccountWidget extends StatelessWidget {
+  final Account account;
+  const AccountWidget({super.key, required this.account});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 128,
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.only(bottom: 8),
+      decoration: BoxDecoration(
+        color: AppColor.lightOrange,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "${account.name} ${account.lastName}",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Text("ID: ${account.id}"),
+              Text("Saldo: ${account.balance.toStringAsFixed(2)}"),
+              Text("Tipo: ${account.accountType ?? "Sem tipo Definido"}"),
+            ],
+          ),
+          IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+        ],
+      ),
+    );
+  }
+}
