114e35d778c8a11cc1816797bdbcef21b5e917ad
Jose A F Santos <toni.farias@outlook.com>
2025-12-21 16:35:21 -0300

    Botão flutuante

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -4,8 +4,21 @@ import 'package:banco_douro/ui/styles/app_colors.dart';
 import 'package:banco_douro/ui/widgets/account_widget.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<List<Account>> _futureGetAll = AccountService().getAll();
+
+  Future<void> refreshGetAll() async {
+    setState(() {
+      _futureGetAll = AccountService().getAll();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,34 +35,42 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: AppColor.orange,
+        child: const Icon(Icons.add, color: Colors.black),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: FutureBuilder(
-          future: AccountService().getAll(),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-                return Center(child: CircularProgressIndicator());
-              case ConnectionState.waiting:
-                return Center(child: CircularProgressIndicator());
-              case ConnectionState.active:
-                return Center(child: CircularProgressIndicator());
-              case ConnectionState.done:
-                {
-                  if (snapshot.data == null || snapshot.data!.isEmpty) {
-                    return Center(child: Text("Nenhuma conta recebida."));
-                  } else {
-                    List<Account> listAccounts = snapshot.data!;
-                    return ListView.builder(
-                      itemCount: listAccounts.length,
-                      itemBuilder: (context, index) {
-                        return AccountWidget(account: listAccounts[index]);
-                      },
-                    );
+        child: RefreshIndicator(
+          onRefresh: refreshGetAll,
+          child: FutureBuilder(
+            future: _futureGetAll,
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return Center(child: CircularProgressIndicator());
+                case ConnectionState.waiting:
+                  return Center(child: CircularProgressIndicator());
+                case ConnectionState.active:
+                  return Center(child: CircularProgressIndicator());
+                case ConnectionState.done:
+                  {
+                    if (snapshot.data == null || snapshot.data!.isEmpty) {
+                      return Center(child: Text("Nenhuma conta recebida."));
+                    } else {
+                      List<Account> listAccounts = snapshot.data!;
+                      return ListView.builder(
+                        itemCount: listAccounts.length,
+                        itemBuilder: (context, index) {
+                          return AccountWidget(account: listAccounts[index]);
+                        },
+                      );
+                    }
                   }
-                }
-            }
-          },
+              }
+            },
+          ),
         ),
       ),
     );
